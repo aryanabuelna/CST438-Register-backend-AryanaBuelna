62e6b86f22eddd1b0e79d16dd25edceb986cee35
aryanabuelna <aryanabuelna0510@gmail.com>
2023-04-26 20:25:13 -0700

    hw5 end to end testing file

---
+++ cst438_register/src/test/java/com/cst438/registrationService.java
@@ -0,0 +1,7 @@
+package com.cst438;
+
+public class registrationService {
+// code an end-to-end test  
+// registration service) as an Admin I can add a new student to the registration service
+
+}
